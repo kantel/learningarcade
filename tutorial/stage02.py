76b3a3b1e98b23c965ca5663ff3aa6c23f1560a9
Jörg Kantel <joerg@kantel.de>
2018-08-11 20:33:12 +0200

    Stage 2 cont.

--- tutorial/stage02.py
+++ tutorial/stage02.py
@@ -3,7 +3,8 @@ import os
 
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
-SCREEN_TITLE = "Arcade Rogue Tutorial Stage 1"
+SCREEN_TITLE = "Arcade Rogue Tutorial Stage 2"
+SPEED = 5
 TILE_SIZE = 32
 t2 = TILE_SIZE/2
 
@@ -25,6 +26,8 @@ class MyGame (ar.Window):
     def setup(self):
         # Sprite-Listen
         self.player_list = ar.SpriteList()
+        self.wall_list = ar.SpriteList()
+        
 
         # Player-Info
         self.player_sprite = ar.Sprite("images/character.png")
@@ -32,23 +35,44 @@ class MyGame (ar.Window):
         self.player_sprite.center_y = 7*TILE_SIZE + t2
         self.player_list.append(self.player_sprite)
         
+        # Mauer
+        wall = ar.Sprite("images/wall.png")
+        wall.center_x = 5*TILE_SIZE + t2
+        wall.center_y = 3*TILE_SIZE + t2
+        self.wall_list.append(wall)
+        
+        wall = ar.Sprite("images/wall.png")
+        wall.center_x = 4*TILE_SIZE + t2
+        wall.center_y = 3*TILE_SIZE + t2
+        self.wall_list.append(wall)
+        
+        # Eine einfache Physic Engine zur Kollisionserkennung
+        self.physics_engine = ar.PhysicsEngineSimple(self.player_sprite, self.wall_list)
+
     def on_draw(self):
         ar.start_render()
-        
         self.player_list.draw()
+        self.wall_list.draw()
     
     def update(self, delta_time):
-        pass
+        self.physics_engine.update()
 
     def on_key_press(self, key, modifiers):
         if key == ar.key.UP:
-            self.player_sprite.center_y += TILE_SIZE
+            self.player_sprite.change_y = SPEED
         elif key == ar.key.DOWN:
-            self.player_sprite.center_y -= TILE_SIZE
+            self.player_sprite.change_y = -SPEED
         elif key == ar.key.LEFT:
-            self.player_sprite.center_x -= TILE_SIZE
+            self.player_sprite.change_x = -SPEED
         elif key == ar.key.RIGHT:
-            self.player_sprite.center_x += TILE_SIZE
+            self.player_sprite.change_x = SPEED
+
+    def on_key_release(self, key, modifiers):
+        if key == ar.key.UP or key == ar.key.DOWN:
+            self.player_sprite.change_y = 0
+        elif key == ar.key.LEFT or key == ar.key.RIGHT:
+            self.player_sprite.change_x = 0
+
 
 
 game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
